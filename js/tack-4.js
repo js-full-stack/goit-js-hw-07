2d34af62cfcdb2287876eba891a3403e34aeacc0
Alex <js.fullstack.development@gmail.com>
2021-01-14 05:37:13 +0300

    hw

--- js/tack-4.js
+++ js/tack-4.js
@@ -5,7 +5,6 @@ const incrementBtnRef = document.querySelector(
   'button[data-action="increment"]'
 );
 const counterValue = document.querySelector("#value");
-console.dir(counterValue);
 
 decrementBtnRef.addEventListener("click", decrementCounterOnClick);
 incrementBtnRef.addEventListener("click", incrementCounterOnClick);
