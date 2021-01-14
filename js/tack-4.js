4db2033e70f16c298b1fc8211073450e3d421c66
Alex <js.fullstack.development@gmail.com>
2021-01-14 05:35:48 +0300

    hw

--- js/tack-4.js
+++ js/tack-4.js
@@ -4,22 +4,16 @@ const decrementBtnRef = document.querySelector(
 const incrementBtnRef = document.querySelector(
   'button[data-action="increment"]'
 );
-
 const counterValue = document.querySelector("#value");
 console.dir(counterValue);
-
-let leadToTheNumberCounterValue = Number(counterValue.textContent);
 
 decrementBtnRef.addEventListener("click", decrementCounterOnClick);
 incrementBtnRef.addEventListener("click", incrementCounterOnClick);
 
 function decrementCounterOnClick(event) {
-  leadToTheNumberCounterValue += Number(event.target.textContent);
-  console.log(leadToTheNumberCounterValue);
-  counterValue.textContent = leadToTheNumberCounterValue;
+  counterValue.textContent += event.target.textContent;
 }
 
 function incrementCounterOnClick(event) {
-  leadToTheNumberCounterValue += Number(event.target.textContent);
-  counterValue.textContent = leadToTheNumberCounterValue;
+  counterValue.textContent += event.target.textContent;
 }
